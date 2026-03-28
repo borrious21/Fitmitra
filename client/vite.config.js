94bd1f01b8f9f8d9566fb30041f60cf76f046664
Parikshit Gautam <parikshit10gautam@gmail.com>
2026-03-28 23:44:40 +0545

    edited vie config

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,13 +3,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [react()],
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000', 
+        target: 'http://localhost:5000',
         changeOrigin: true,
       }
     }
